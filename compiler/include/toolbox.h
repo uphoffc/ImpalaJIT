f8abae8c1ebb55e7052fe58ed3a9bd2dff8b4e56
ravil <ravil.dorozhinskii@tum.de>
2021-06-03 20:24:45 +0200

    Added a simple symbol table

--- compiler/include/toolbox.h
+++ compiler/include/toolbox.h
@@ -4,10 +4,52 @@
 #include "engine_types.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Value.h"
+#include <sstream>
 #include <string>
 #include <unordered_map>
+#include <vector>
+#include <algorithm>
 
 namespace impala {
+
+struct SymbolTable {
+  SymbolTable() = default;
+
+  void removeScope() { scopes.pop_back(); }
+  void addScope() { scopes.push_back(TableT{});}
+  size_t getNumScopes() {return scopes.size();}
+
+  llvm::Value* findSymbol(std::string name) {
+    for (auto scope = scopes.rbegin(); scope != scopes.rend(); ++scope) {
+      if (scope->find(name) != scope->end()) {
+        return (*scope)[name];
+      }
+    }
+    return nullptr;
+  }
+
+  void addSymbol(std::string name, llvm::Value* value) {
+    if (scopes.empty()) {
+      this->addScope();
+    }
+    auto& topMostScope = scopes.back();
+    if (topMostScope.find(name) != topMostScope.end()) {
+      std::stringstream errStream;
+      errStream << "symbol `" << name << "` has already been defined in this scope";
+      throw std::runtime_error(errStream.str());
+    }
+    else {
+      topMostScope[name] = value;
+    }
+  }
+
+  using TableT = std::unordered_map<std::string, llvm::Value*>;
+
+private:
+  std::vector<TableT> scopes{};
+};
+
+
 using TableT = std::unordered_map<std::string, llvm::Value *>;
 struct Toolbox {
   Toolbox(llvm::LLVMContext &context, llvm::IRBuilder<> &builder, engine::types::FunctionProtosT &externalMathFunctions)
